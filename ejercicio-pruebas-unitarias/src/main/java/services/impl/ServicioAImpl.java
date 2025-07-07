11380a212e3b89d40bde9c47e658ad9d1184c67a
JoanGarciaBild <joangarciabild>
2025-07-07 14:16:41 -0700

    TDD and Mockito excercise

---
+++ ejercicio-pruebas-unitarias/src/main/java/services/impl/ServicioAImpl.java
@@ -0,0 +1,12 @@
+package services.impl;
+
+import services.ServicioA;
+
+public class ServicioAImpl implements ServicioA {
+
+
+    @Override
+    public int sumar(int a, int b) {
+        return a+b;
+    }
+}
